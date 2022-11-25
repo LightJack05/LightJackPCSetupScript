0144580e33202b388aba1feedce38024c1dd31fe
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2022-11-26 00:11:00 +0100

    Attempt at one-liner script

--- DownloadRun.bat
+++ DownloadRun.bat
@@ -1,2 +1,10 @@
 # Intended to run the script with just a one-liner.
 
+mkdir %temp%\SetupScript
+curl https://raw.githubusercontent.com/LightJack05/LightJackPCSetupScript/main/main.ps1 -o %temp%\SetupScript\main.ps1
+curl https://raw.githubusercontent.com/LightJack05/LightJackPCSetupScript/main/RunMeAsAdmin.bat -o %temp%\SetupScript\RunMeAsAdmin.bat
+%temp%\SetupScript\RunMeAsAdmin.bat
+
+sleep 3
+
+rmdir -r %temp%\SetupScript
--- RunMeAsAdmin.bat
+++ RunMeAsAdmin.bat
@@ -1,3 +1,4 @@
 @echo off
 powershell.exe -ExecutionPolicy Bypass -File %0\..\main.ps1
+rmdir -r %temp%\SetupScript
 Pause
