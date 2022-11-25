d450d50e7fbb927bf0e142235b27f6c7a24bb40e
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2022-11-26 00:31:32 +0100

    Switched to PS to use IEX

---
+++ DownloadRun.ps1
@@ -0,0 +1,10 @@
+# Intended to run the script with just a one-liner.
+
+mkdir %temp%\SetupScript
+curl https://raw.githubusercontent.com/LightJack05/LightJackPCSetupScript/main/main.ps1 -o $env:TEMP\SetupScript\main.ps1
+curl https://raw.githubusercontent.com/LightJack05/LightJackPCSetupScript/main/RunMeAsAdmin.bat -o $env:TEMP\SetupScript\RunMeAsAdmin.bat
+Start-Process $env:TEMP\SetupScript\RunMeAsAdmin.bat
+
+Start-Sleep 3
+
+Remove-Item -r %temp%\SetupScript
