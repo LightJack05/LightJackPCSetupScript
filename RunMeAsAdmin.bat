0642ba34456d09001b762ae65f3b026854ea682c
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2022-11-25 23:11:39 +0100

    Automated winget setup and added careless mode

--- RunMeAsAdmin.bat
+++ RunMeAsAdmin.bat
@@ -1,2 +1,3 @@
 @echo off
-powershell.exe -ExecutionPolicy Bypass -File main.ps1
+powershell.exe -ExecutionPolicy Bypass -File %0\..\main.ps1 -Careless
+Pause
--- main.ps1
+++ main.ps1
@@ -6,62 +6,113 @@
 # It should be run as admin to avoid interruptions by UAC prompts, but doesn't have to.
 # It requires the winget package manager to be installed at runtime or before starting.
 #
+# I'm a beginner in terms of PowerShell Scripting, please don't be too hard on me. ;)
 #
 # Setup Script by LightJack05
-
-
-
+param (
+    [switch]$Careless = $false
+)
 
 function Main {
+    #Function called on execution
     Write-Output "Welcome! Please press any key to begin setup..."
-    Clear-Host
     $Host.UI.RawUI.ReadKey()
+    Clear-Host
+    if ($AvoidHashChecks) {
+        Write-Host 'WARNING: Running in careless mode. There is no check if the downloaded file is damaged or malicious. There is also no version checking. Please make sure software is up to date once installed.' -ForegroundColor red
+        SetupMachine
+    }
+    else {
+        # Check if winget needs to be installed
+        if (CheckForWinget) {
+            SetupMachine
+        }
+        else {
+            Write-Host 'Updating store software to aquire winget...'
+            KickStartUpdate
+            WaitForWinget
+        }
+    }
+
+}
+
+function CheckForWinget {
 
     $cmdName = "winget"
     if (Get-Command $cmdName -errorAction SilentlyContinue) {
-        Clear-Host
-        Write-Output 'Winget found!'
+        Return $true
     }
     else {
-        Write-Host 'This script requires the latest version of the Windows package manager "winget", which was not found on this machine.'
-        Write-Host 'To install it, please install the latest updates from the Microsoft Store.'
-        Write-Host ''
-        Write-Host 'Follow the appropriate instructions for your Winodws Store Version:'
-        Write-Host 'Old Ms Store (Default Included Version 11910.1002.5.0):'
-        Write-Host 'Go To ... > Downloads and Updates > Get Updates'
-        Write-Host 'New Ms Store (Currently Version 22210.1401.6.0 or later):'
-        Write-Host 'Go To Library > Get Updates'
-        Write-Host ''
-        Write-Host 'This script will wait until winget is found...'
-        Write-Host ''
-        WaitForWinget
+        Return $false
     }
+}
+
+function KickStartUpdate {
+    # Open MS Store on app installer page to update it.
+    Write-Output 'Opening Windows Store to kickstart app-updates.'
+    Write-Output 'Please click "Update" on the window that pops up.'
+    Start-Process ms-windows-store://pdp/?ProductId=9NBLGGH4NNS1
+    Start-Sleep 8
+    #Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
 }
 
 function WaitForWinget {
+    # Loop until winget is found.
+    Write-Host 'The script will continue when winget is found. Please wait.'
     while ($true) {
-        Start-Sleep 5
-        $cmdName = "winget"
-        if (Get-Command $cmdName -errorAction SilentlyContinue) {
+        Start-Sleep 1
+        if (CheckForWinget) {
             Clear-Host
             Write-Output 'Winget Found!'
+            Write-Output 'Killing Windows Store...'
+            taskkill.exe /f /IM WinStore.App.exe
             SetupMachine
+            break
         }
-
     }
-
 }
 
 function SetupMachine {
-
     Write-Output 'Initiating Setup...'
     Write-Output 'Installing Software'
-    winget install Google.Chrome
+    #
+    # IMPORTANT
+    #
+    # In regular operation you should disable "AvoidHashChecks".
+    # This is intended as a temporary solution if hashes are creating problems.
+    # Usually these problems are resolved within a couple hours or at most days.
+
+    if ($Careless) {
+        # Chrome
+        Write-Host "Installing Chrome..."
+        curl https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BC253142F-0FB0-E40A-E37B-336427084533%7D%26lang%3Den%26browser%3D5%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe -o $env:TEMP\ChromeSetup.exe
+        Start-Process $env:TEMP\ChromeSetup.exe
+
+
+        Write-Host "Installing Chrome..."
+        curl https://7-zip.org/a/7z2201-x64.msi -o $env:TEMP\7zip.msi
+        Start-Process $env:TEMP\7zip.msi /quiet
+
+
+        Write-Host 'Waiting 4 minutes for setups to complete to clean up...'
+        Start-Sleep 240
+        Remove-Item $env:TEMP\7zip.msi
+        Remove-Item $env:TEMP\ChromeSetup.exe
+    }
+    else {
+        winget install Google.Chrome --accept-source-agreements --accept-package-agreements
+        winget install 7zip.7zip --source winget --accept-source-agreements --accept-package-agreements
+    }
+
+
+
 
     Write-Output 'Changing Settings...'
 
+    Write-Output 'Setup has been completed. Press any key to exit.'
 }
 
+Main
 
 
 
