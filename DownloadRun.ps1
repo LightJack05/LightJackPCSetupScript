383cfb1691d3028954ef09e90f915e0f2035171f
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2022-11-26 00:33:18 +0100

    Added command and fixed path issue

--- DownloadRun.ps1
+++ DownloadRun.ps1
@@ -1,6 +1,8 @@
 # Intended to run the script with just a one-liner.
+# Run this command:
+# $Command = curl https://raw.githubusercontent.com/LightJack05/LightJackPCSetupScript/main/DownloadRun.ps1 -UseBasicParsing; Invoke-Expression $Command
 
-mkdir %temp%\SetupScript
+mkdir $env:TEMP\SetupScript
 curl https://raw.githubusercontent.com/LightJack05/LightJackPCSetupScript/main/main.ps1 -o $env:TEMP\SetupScript\main.ps1
 curl https://raw.githubusercontent.com/LightJack05/LightJackPCSetupScript/main/RunMeAsAdmin.bat -o $env:TEMP\SetupScript\RunMeAsAdmin.bat
 Start-Process $env:TEMP\SetupScript\RunMeAsAdmin.bat
