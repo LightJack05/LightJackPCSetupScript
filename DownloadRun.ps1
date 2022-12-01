ebabe14ee0b084d4fe194c3ec17bf6893c8bf1b8
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2022-12-01 17:11:21 +0100

    fixed copy-paste error

--- DownloadRun.ps1
+++ DownloadRun.ps1
@@ -4,7 +4,7 @@
 
 mkdir $env:TEMP\SetupScript
 curl https://raw.githubusercontent.com/LightJack05/LightJackPCSetupScript/separate-setup-script/main.ps1 -o $env:TEMP\SetupScript\main.ps1
-curl https://raw.githubusercontent.com/LightJack05/LightJackPCSetupScript/separate-setup-script/RunMeAsAdmin.bat -o $env:TEMP\SetupScript\setup.ps1
+curl https://raw.githubusercontent.com/LightJack05/LightJackPCSetupScript/separate-setup-script/setup.ps1 -o $env:TEMP\SetupScript\setup.ps1
 curl https://raw.githubusercontent.com/LightJack05/LightJackPCSetupScript/separate-setup-script/RunMeAsAdmin.bat -o $env:TEMP\SetupScript\RunMeAsAdmin.bat
 Start-Process $env:TEMP\SetupScript\RunMeAsAdmin.bat
 
